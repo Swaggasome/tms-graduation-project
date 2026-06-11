f5a9dc46fdc627ebaf552281640ee05723e9119d
Swaggasome <4galkin@gmail.com>
2026-06-11 08:30:43 +0300

    Add calendar export URL

--- apps/bookings/urls.py
+++ apps/bookings/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.calendar_view, name='calendar'),
     path('create/', views.create_booking, name='create_booking'),
+    path('export/', views.export_calendar_ics, name='export_calendar'),
     path('api/events/', views.get_events_api, name='api_events'),
 ]
