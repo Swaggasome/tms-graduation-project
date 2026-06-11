6ca17d51a895db5f0029142e17aa659c8c18c27a
Swaggasome <4galkin@gmail.com>
2026-06-11 09:39:16 +0300

    Add calendar feed URL route

--- apps/bookings/urls.py
+++ apps/bookings/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.calendar_view, name='calendar'),
     path('create/', views.create_booking, name='create_booking'),
     path('export/', views.export_calendar_ics, name='export_calendar'),
+    path('feed/<str:token>.ics', views.calendar_feed, name='calendar_feed'),
     path('api/events/', views.get_events_api, name='api_events'),
 ]
